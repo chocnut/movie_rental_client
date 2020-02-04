a5b23a227fef3d80796bf7ac88dfef4d4e82d126
Peter Indiola <peter@incube8.sg>
2020-02-04 23:37:44 +0800

    Setup react router and initial page components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "../routes/PrivateRoute";
+import { Login, Movies } from "../pages";
 
-const App = () => {
-    return <div>Hello</div>
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <PrivateRoute path="/movies">
+        <Movies />
+      </PrivateRoute>
+    </Switch>
+  </Router>
+);
 
-export default App
+export default App;
---
+++ src/pages/movies/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Container = () => <div>Movies</div>;
+
+export default Container;
